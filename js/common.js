5d47ba11ee503e9e38199d060492b89dc801af1b
Anna-Khvatova <49308812+Anna-Khvatova@users.noreply.github.com>
2025-04-22 15:01:50 +0300

    modification

--- js/common.js
+++ js/common.js
@@ -128,13 +128,13 @@ document.addEventListener('DOMContentLoaded', () => {
       },
     });
 
-    let hoverContainer = document.querySelector('.first-screen__content-right');
-    hoverContainer.addEventListener('mouseover', function () {
-        document.querySelector('.first-screen').classList.add('hover-container');
-    });
-    hoverContainer.addEventListener('mouseleave', function () {
-        document.querySelector('.first-screen').classList.remove('hover-container');
-    });
+    // let hoverContainer = document.querySelector('.first-screen__content-right');
+    // hoverContainer.addEventListener('mouseover', function () {
+    //     document.querySelector('.first-screen').classList.add('hover-container');
+    // });
+    // hoverContainer.addEventListener('mouseleave', function () {
+    //     document.querySelector('.first-screen').classList.remove('hover-container');
+    // });
 
     $(function () {
         $(window).scroll(function () {
@@ -220,3 +220,61 @@ document.addEventListener('DOMContentLoaded', () => {
       keepHighlightUntilNext: true
   });
 });
+// $(window).scroll(function(e){
+//     parallax();
+//   });
+//   function parallax(){
+//     var scrolled = $(window).scrollTop();
+//     $('.first-screen__bg').css('top',-(scrolled*0.2)+'px');
+// }
+// makes the parallax elements
+function parallaxIt() {
+  // create variables
+  var $fwindow = $(window);
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  var $contents = [];
+  var $backgrounds = [];
+
+  // for each of content parallax element
+  $('[data-type="content"]').each(function(index, e) {
+    var $contentObj = $(this);
+
+    $contentObj.__speed = ($contentObj.data('speed') || 1);
+    $contentObj.__fgOffset = $contentObj.offset().top;
+    $contents.push($contentObj);
+  });
+
+  // for each of background parallax element
+  $('[data-type="background"]').each(function() {
+    var $backgroundObj = $(this);
+
+    $backgroundObj.__speed = ($backgroundObj.data('speed') || 1);
+    $backgroundObj.__fgOffset = $backgroundObj.offset().top;
+    $backgrounds.push($backgroundObj);
+  });
+
+  // update positions
+  $fwindow.on('scroll resize', function() {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    $contents.forEach(function($contentObj) {
+      var yPos = $contentObj.__fgOffset - scrollTop / $contentObj.__speed;
+
+      $contentObj.css('top', yPos);
+    })
+
+    $backgrounds.forEach(function($backgroundObj) {
+      var yPos = -((scrollTop - $backgroundObj.__fgOffset) / $backgroundObj.__speed);
+
+      $backgroundObj.css({
+        backgroundPosition: '50% ' + yPos + 'px'
+      });
+    });
+  });
+
+  // triggers winodw scroll for refresh
+  $fwindow.trigger('scroll');
+};
+
+parallaxIt();
